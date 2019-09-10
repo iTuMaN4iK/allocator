29f86674a4b5e352856d1886950d344ced5e7f7e
iTuMaN4iK <swaga.ituman4ik@gmail.com>
2019-09-10 21:11:40 +0500

    test deploy

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "lib.h"
+#include <cassert>
+#include <cstdlib>
+#include <algorithm>
+
+#define UNUSED(variable) (void)variable
+int main(int argc, char const *argv[])
+{
+    UNUSED(argc);
+    UNUSED(argv);
+    return 0;
+}
